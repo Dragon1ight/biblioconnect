4e7a9a17e6f65e649c1d0abd41bd7efe2e65e01f
Khalidzaim <118683202+Dragon1ight@users.noreply.github.com>
2025-07-16 09:22:08 +0100

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,31 +1,31 @@
 from locust import HttpUser, task, between
-
+ 
 class BiblioUser(HttpUser):
     wait_time = between(1, 3)
-
+ 
     @task(2)
     def homepage(self):
-        self.client.get("/", verify=False)
-
+        self.client.get("/")
+ 
     @task(3)
     def list_books(self):
-        self.client.get("/book/", verify=False)
-
+        self.client.get("/book/")
+ 
     @task(2)
     def book_detail(self):
-        self.client.get("/book/3", verify=False) 
-
+        self.client.get("/book/3")
+ 
     @task(1)
     def view_reservations(self):
-        self.client.get("/reservation/", verify=False)
-
+        self.client.get("/reservation/")
+ 
     @task(1)
     def login(self):
         self.client.post("/login", {
             "email": "user@biblioconnect.fr",
-            "password": "password" 
-        }, verify=False)
-
+            "password": "password"
+        })
+ 
     @task(1)
     def register(self):
         self.client.post("/register", {
@@ -34,4 +34,4 @@ class BiblioUser(HttpUser):
             "first_name": "Test",
             "last_name": "User",
             "address": "Paris"
-        }, verify=False)
+        })
